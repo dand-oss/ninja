a43ba017dff5a017d7230a331000d4c71b2ac4c3
Dan Dees <dand@appsmiths.com>
2020-01-24 22:53:02 +0700

    build

--- buildit.sh
+++ buildit.sh
@@ -1,4 +1,9 @@
-mkdir the-build
-cd the-build
-cmake -Bbuild-cmake --Build=Release -H ..
+# out of source
+mkdir release
+cd release
+
+# gen build
+cmake -Bbuild-cmake -DCMAKE_BUILD_TYPE=Release  -H. ..
+
+# biuld it
 cmake --build build-cmake
